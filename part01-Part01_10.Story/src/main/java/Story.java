7dc278cabfb2973d82916056dac26d85c4db5cdb
me <67919144+FBaia2@users.noreply.github.com>
2024-03-12 16:33:26 +0000

    Completed Story.java without a sweat

--- part01-Part01_10.Story/src/main/java/Story.java
+++ part01-Part01_10.Story/src/main/java/Story.java
@@ -4,9 +4,17 @@ import java.util.Scanner;
 public class Story {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("I will tell you a story, but I need some information first.");
+        System.out.println("What is the main character called?");
+        String name = s.nextLine();
+        System.out.println("What is their job?");
+        String job = s.nextLine();
+        System.out.println("Here is the story:");
+        System.out.println("Once upon a time there was " + name + ", who was " + job + ".");
+        System.out.println("On the way to work, " + name + " reflected on life.");
+        System.out.println("Perhaps " + name + " will not be " + job + " forever.");
 
     }
 }
