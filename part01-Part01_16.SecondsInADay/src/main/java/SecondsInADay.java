ee46d5530f081cdcd5bea259ab44f2944b185de1
me <67919144+FBaia2@users.noreply.github.com>
2024-03-12 23:16:26 +0000

    initialized repo

--- part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -5,6 +5,10 @@ public class SecondsInADay {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("How many days would you like to convert to seconds?");
+        int tger = Integer.valueOf(scanner.nextLine());
+        int tgerdays = tger * 86400;
+        System.out.println(tgerdays);
 
         // Write your program here
 
--- part01-Part01_18.SumOfThreeNumbers/src/main/java/SumOfThreeNumbers.java
+++ part01-Part01_18.SumOfThreeNumbers/src/main/java/SumOfThreeNumbers.java
@@ -5,6 +5,13 @@ public class SumOfThreeNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int sum1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int sum2 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the third number:");
+        int sum3 = Integer.valueOf(scanner.nextLine());
+        System.out.println("The sum of the numbers is " + (sum1 + sum2 + sum3));
 
         // Write your program here
 
