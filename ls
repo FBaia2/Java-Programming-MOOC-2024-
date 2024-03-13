3342b4156486015d5daf2f2f77caa7cbd837af57
me <67919144+FBaia2@users.noreply.github.com>
2024-03-13 13:21:45 +0000

    "init repo"

---
+++ ls
@@ -0,0 +1,17 @@
+[33mcommit ee46d5530f081cdcd5bea259ab44f2944b185de1[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m)[m
+Author: me <67919144+FBaia2@users.noreply.github.com>
+Date:   Tue Mar 12 23:16:26 2024 +0000
+
+    initialized repo
+
+[33mcommit 7dc278cabfb2973d82916056dac26d85c4db5cdb[m[33m ([m[1;31morigin/main[m[33m)[m
+Author: me <67919144+FBaia2@users.noreply.github.com>
+Date:   Tue Mar 12 16:33:26 2024 +0000
+
+    Completed Story.java without a sweat
+
+[33mcommit 95471c3eb51fa159ff240fd5e608f03408c7aed5[m
+Author: me <67919144+FBaia2@users.noreply.github.com>
+Date:   Tue Mar 12 10:36:56 2024 +0000
+
+    intialized repository
--- part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -5,6 +5,20 @@ public class SimpleCalculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int num1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int num2 = Integer.valueOf(scanner.nextLine());
+
+        int plus = num1 + num2;
+        int minus = num1 - num2;
+        int multi = num1 * num2;
+        double divi = (double) num1 / num2;
+
+        System.out.println(num1 + " + " + num2 + " = " + plus);
+        System.out.println(num1 + " - " + num2 + " = " + minus);
+        System.out.println(num1 + " * " + num2 + " = " + multi);
+        System.out.println(num1 + " / " + num2 + " = " + divi);
 
         // Write your program here
 
