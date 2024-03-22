b9653e46d009dc81d076edf8d9ddd7052396b59c
me <67919144+FBaia2@users.noreply.github.com>
2024-03-22 10:05:58 +0000

    why so ez

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,23 @@
 
 public class HealthStation {
+    private int counter;
 
 
     public int weigh(Person person) {
-        // return the weight of the person passed as the parameter
-        return -1;
+        counter ++;
+        return person.getWeight();
+        
+    }
+
+    public void feed(Person person) {
+        person.setWeight(weigh(person)+1);
+
+    }
+
+
+    public int weighings() {
+        return counter;
+
     }
 
 }
